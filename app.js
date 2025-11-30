5fb2ade5cd005b3e41ad61083e5605ea8496be45
Sandeep Jadhav <sandepdj11@gmail.com>
2025-11-30 13:54:15 +0530

    formatted

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {  
-res.send('Hello welcome to my node application');
+  res.send('Hello welcome to my node application');
 });
 
 app.listen(port, () => {  
-console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
