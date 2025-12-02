51138eb4e0bf1584b0ec47257acca90ba1180fa1
Sandeep Jadhav <sandepdj11@gmail.com>
2025-12-02 12:06:59 +0530

    node logger

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 // app.js
 const express = require('express');
-const pino = require('pino-http')()
-
+const logger = require('./logger'); // Import your logger
 
 const app = express();
 const port = 3000;
 
-app.use(pino);
 
 app.get('/', (req, res) => {  
-  req.log.info('Root API called');
+  logger.info('Root API called');
   res.send('Hello welcome to my node application');
 });
 
 app.get('/health', (req, res) => {
-  req.log.info('This is the health route')
+  logger.info('This is the health route')
   res.send('This is the health route');
 });
 
 app.post('/users', (req, res) => {
-  req.log.info(`POST users API called ${JSON.stringify(req?.body)}`);
+  logger.info(`POST users API called ${JSON.stringify(req?.body)}`);
   res.send('This is the health route');
 });
 
 app.listen(port, () => {  
-  console.log(`Example app listening at http://localhost:${port}`);
+  logger.info(`Example app listening at http://localhost:${port}`);
 });
