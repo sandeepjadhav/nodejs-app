e7a8f9ce466dfbb1a6ea218a9d593972730b200b
Sandeep Jadhav <sandepdj11@gmail.com>
2025-12-02 11:21:01 +0530

    Health route updated

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {  
+  console.log(`Root API called....`);
   res.send('Hello welcome to my node application');
 });
+
+app.get('/health', (req, res) => {
+  console.log(`This is the health route`);
+  res.send('This is the health route');
+});
+
 
 app.listen(port, () => {  
   console.log(`Example app listening at http://localhost:${port}`);
